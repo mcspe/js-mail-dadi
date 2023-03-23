e45f32c2eae880b45ac675c179484ccbfecc4224
mcspe <mcspezzaferro@gmail.com>
2023-03-23 17:16:09 +0100

    [add] basic graphic display

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -11,21 +11,32 @@ function randomThrow(){
 
 button.addEventListener('click', function(){
 
+  if (document.querySelector('.result').classList.contains('d-none')) document.querySelector('.result').classList.remove('d-none');
+  document.querySelector('.result .text').innerHTML = '';
+
   let computerTurn = randomThrow();
   let userTurn = randomThrow();
   let message;
 
   if (userTurn > computerTurn){
-    message = 'hai vinto';
+    message = 'Complimenti! Hai vinto';
   } else if(computerTurn > userTurn){
-    message = 'hai perso';
+    message = 'Peccato! Hai perso';
   } else{
-    message = 'patta';
+    message = 'Patta! Lancia un\'altra volta';
   }
 
-  console.log(`
-  hai estratto ${dado[userTurn]}, il computer ha estratto ${dado[computerTurn]}
-  ${message}
-  `);
+  const userDice = document.createElement('div');
+  userDice.append('TU');
+  userDice.append(dado[userTurn]);
+
+  const computerDice = document.createElement('div');
+  computerDice.append('COMPUTER');
+  computerDice.append(dado[computerTurn]);
+
+  const resultMessage = document.createElement('p');
+  resultMessage.append(message);
+
+  document.querySelector('.result .text').append(userDice, computerDice, resultMessage);
 
 });
