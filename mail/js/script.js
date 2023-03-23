f522f90af4dfdbbc08438d60c82237e10c66d4d4
mcspe <mcspezzaferro@gmail.com>
2023-03-23 16:21:18 +0100

    [add] get user name function and display

--- mail/js/script.js
+++ mail/js/script.js
@@ -12,9 +12,19 @@ let access = false;
 
 const button = document.querySelector('.container .card button');
 
+function getName(mail){
+  const mailArr = mail.split('.');
+  let name = mailArr[0];
+  mailArr.pop();
+  const lastName = mailArr[1].split('@');
+  name += ' ' + lastName[0];
+  return name;
+}
+
 button.addEventListener('click', function(){
 
   const userMail = document.querySelector('.container .card input');
+  const userName = getName(userMail.value);
 
   if (document.querySelector('.result').classList.contains('d-none')) document.querySelector('.result').classList.remove('d-none');
 
@@ -22,7 +32,6 @@ button.addEventListener('click', function(){
     if (userMail.value === authUserList[i]){
       i = authUserList.length;
       access = true;
-      console.log('si');
     } else {
       access = false;
     }
@@ -32,6 +41,9 @@ button.addEventListener('click', function(){
     document.querySelector('.result .negative').classList.add('d-none');
     document.querySelector('.result .positive').classList.remove('d-none');
     userMail.value = '';
+    document.querySelector('.user').style.fontWeight = "bold";
+    document.querySelector('.user').style.textTransform = "capitalize";
+    document.querySelector('.user').append(userName);
   } else{
     document.querySelector('.result .positive').classList.add('d-none');
     document.querySelector('.result .negative').classList.remove('d-none');
